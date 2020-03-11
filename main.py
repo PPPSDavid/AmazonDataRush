ca6d3c7a92022ad63645fd354b93302cbd6d1c7e
David Yu <39285150+PPPSDavid@users.noreply.github.com>
2020-03-11 12:08:37 -0400

    Phase one finished, item page extraction in process.

--- main.py
+++ main.py
@@ -54,6 +54,13 @@ from itertools import cycle
 # proxy_pool = cycle(get_proxies())
 
 BASE_URL = "https://www.amazon.com/s?k="
+HEADER = {
+    "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "cache-control": "max-age=0",
+}
 
 
 def get_search_page(item_name, pg):
@@ -65,13 +72,7 @@ def get_search_page(item_name, pg):
             time.sleep(random.randint(1, 3))
             # proxy = next(proxy_pool)
             response = requests.get(
-                target, headers={
-                    "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-                    "cache-control": "max-age=0",
-                })
+                target, headers=HEADER)
             if response.status_code != 200:
                 # print("Error!")
                 # Request is not successfully handled
@@ -99,16 +100,16 @@ def parse_and_select(res):
     html = etree.HTML(res)
     items_with_all_data = './/div[@class="sg-col-inner"]/div[2]//span[@class="a-price-whole"]/../../../../../../../../../../div[@class="sg-row"][1]//span[@aria-label][1]/../../../../../../../../..'
     item_titles = html.xpath(
-        items_with_all_data+'//span[@class="a-size-medium a-color-base a-text-normal"]/node()')
+        items_with_all_data + '//span[@class="a-size-medium a-color-base a-text-normal"]/node()')
     item_prices = html.xpath(
-        items_with_all_data+'//span[@class="a-price"]/span[@class="a-offscreen"]/node()')
+        items_with_all_data + '//span[@class="a-price"]/span[@class="a-offscreen"]/node()')
     item_url = html.xpath(
-        items_with_all_data+"//a[@class='a-link-normal a-text-normal']/@href")
+        items_with_all_data + "//a[@class='a-link-normal a-text-normal']/@href")
     item_review = html.xpath(
-        items_with_all_data+'//div[@class="sg-row"][1]//span[@aria-label][1]/@aria-label'
+        items_with_all_data + '//div[@class="sg-row"][1]//span[@aria-label][1]/@aria-label'
     )
     item_review_count = html.xpath(
-        items_with_all_data+'//div[@class="sg-row"][1]//span[@aria-label][2]/@aria-label'
+        items_with_all_data + '//div[@class="sg-row"][1]//span[@aria-label][2]/@aria-label'
     )
     print(len(item_titles))
     print(len(item_prices))
@@ -117,7 +118,7 @@ def parse_and_select(res):
     print(len(item_review_count))
     url_processed = []
     for i in item_url:
-        url_processed.append("www.amazon.com"+i)
+        url_processed.append("www.amazon.com" + i)
 
     review_processed = []
     for i in item_review:
@@ -130,14 +131,82 @@ def parse_and_select(res):
 
     data = []
     for i in range(len(item_titles)):
-        item = [item_titles[i], item_prices[i], url_processed[i],review_processed[i], review_count_processed[i]]
+        item = [item_titles[i], item_prices[i], url_processed[i], review_processed[i], review_count_processed[i]]
         data.append(item)
     return data
 
 
-def to_csv(data,name):
+# Phase two: loop through item pages for specific information.
+def get_item_page(url):
+    for i in range(100):
+        try:
+            print("Request #%d" % i)
+            time.sleep(random.randint(1, 3))
+            response = requests.get(
+                url, headers=HEADER)
+            if response.status_code != 200:
+                # print("Error!")
+                # Request is not successfully handled
+                print('Request is not successfully handled')
+                continue
+            elif response.status_code == 200:
+                # print(response.text)
+                html = etree.HTML(response.text)
+                item_titles = html.xpath(
+                    '// div[ @ id = "centerCol"]'
+                )
+                if len(item_titles) == 0:
+                    # Request is blocked by bot detection
+                    print('Request is blocked by bot detection')
+                    continue
+                print("HTML is normal, proceeding to next stage")
+                return response.text
+            return None
+        except:
+            print("Exception,skipping")
+            continue
+    return None
+
+def parse_and_select_item(res):
+    html = etree.HTML(res)
+    item_seller = html.xpath(
+        '//div[@id = "centerCol"]/div[@id="bylineInfo_feature_div"]//a/node()'
+    )
+    item_specs = html.xpath(
+        '//div[@id="twister_feature_div"]//ul/li/@title'
+    )
+    item_description = html.xpath(
+        '//div[@id = "centerCol"]//div[@id="featurebullets_feature_div"]//ul/li[position()>1]/span/node()'
+    )
+    # Should be non-zero for prime supported shipping
+    item_prime = html.xpath(
+        '//span[@id="priceBadging_feature_div"]'
+    )
+    item_stock_level = html.xpath(
+        '//div[@id="availability"]/span/node()'
+    )
+    item_dimension = html.xpath(
+        '//table[@id="productDetails_detailBullets_sections1"]/tbody/tr/th[contains(string(),"Dimension")]/../td/text()'
+    )
+    item_weight = html.xpath(
+        '//table[@id="productDetails_detailBullets_sections1"]/tbody/tr/th[contains(string(),"Item Weight")]/../td/text()'
+    )
+    item_asin= html.xpath(
+        '//table[@id="productDetails_detailBullets_sections1"]/tbody/tr/th[contains(string(),"ASIN")]/../td/text()'
+    )
+    item_ranking = html.xpath(
+        '//table[@id="productDetails_detailBullets_sections1"]/tbody/tr/th[contains(string(),"Best Sellers Rank")]/../td/span/span//text()'
+    )
+    item_first_available = html.xpath(
+        '//table[@id="productDetails_detailBullets_sections1"]/tbody/tr/th[contains(string(),"Date First Available")]/../td/text()'
+    )
+
+
+
+
+def to_csv(data, name):
     headers = ["Name", "Price", "Link", "Customer Review", "Review Count"]
-    csv_file = open("./data/ListSearch_"+name+".csv", "w")
+    csv_file = open("./data/ListSearch_" + name + ".csv", "w")
     writer = csv.writer(csv_file)
     writer.writerow(headers)
     for i in data:
@@ -150,7 +219,7 @@ def main(name, page):
     html = get_search_page(name, page)
     data = parse_and_select(html)
     # print(data)
-    to_csv(data,name)
+    to_csv(data, name)
     print("finished!")
 
 
